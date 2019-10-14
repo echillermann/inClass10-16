213f7b15c17291d437073c51d183fdd2470a9f17
Emma Hillermann <emma.c.hillermann-1@ou.edu>
2019-10-14 14:06:23 -0500

    actually saved file

--- oct9class/Student.java
+++ oct9class/Student.java
@@ -1,5 +1,29 @@
 package oct9class;
 
-public class Student {
+public class Student extends Person implements Comparable<Student>
+{
+	private Integer rating;
+	private String className;
 
+	public Student() throws NameException
+	{
+		super();
+	}
+	
+	public Student(String firstName, String lastName) throws NameException
+	{
+		super(firstName, lastName);
+	}
+	
+	public Student(String firstName, String lastName, int rating) throws NameException
+	{
+		super(firstName, lastName);
+		this.rating = rating;
+	}
+	
+	public int compareTo(Student o)
+	{
+		
+		return 0;
+	}
 }
