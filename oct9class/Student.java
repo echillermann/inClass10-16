0ea9df8c7db0a93b91ee61776265c818cb99ddf7
Emma Hillermann <emma.c.hillermann-1@ou.edu>
2019-10-14 14:22:27 -0500

    added static final variable initial-rating

--- oct9class/Student.java
+++ oct9class/Student.java
@@ -4,21 +4,30 @@ public class Student extends Person implements Comparable<Student>
 {
 	private Integer rating;
 	private String className;
+	public static final int INITIAL_RATING = 0;
+
 
 	public Student() throws NameException
 	{
 		super();
+		rating = INITIAL_RATING;
 	}
 	
 	public Student(String firstName, String lastName) throws NameException
 	{
 		super(firstName, lastName);
+		rating = INITIAL_RATING;
 	}
 	
 	public Student(String firstName, String lastName, int rating) throws NameException
 	{
 		super(firstName, lastName);
 		this.rating = rating;
+	}
+	
+	public Integer getRating()
+	{
+		return rating;
 	}
 	
 	public int compareTo(Student o)
