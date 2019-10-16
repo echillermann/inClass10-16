ae1056b5afd7ee9fe9a7e1889a800a3faf068069
Emma Hillermann <emma.c.hillermann-1@ou.edu>
2019-10-16 14:45:55 -0500

    implemented compare method

--- oct9class/CompareLastFirstName.java
+++ oct9class/CompareLastFirstName.java
@@ -1,5 +1,29 @@
 package oct9class;
 
-public class CompareLastFirstName {
+import java.util.Comparator;
 
+public class CompareLastFirstName implements Comparator<Student>
+{
+	public int compare(Student first, Student second)
+	{
+		String firstName1 = first.getFirstName();
+		String lastName1 = first.getLastName();
+		String firstName2 = second.getFirstName();
+		String lastName2 = second.getLastName();
+		
+		if(firstName1.compareTo(firstName2) < 0)
+			return -1;
+		else if (firstName1.compareTo(firstName2) > 0)
+			return 1;
+		else
+		{
+			if(lastName1.compareTo(lastName2) < 0)
+				return -1;
+			else if(lastName1.compareTo(lastName2) > 0)
+				return 1;
+			else
+				return 0;
+		}
+		
+	}
 }
